52510b581c51817159939a1e42a4bb833125cb10
Nickolay Grechkin <nickolaygrechkin@gmail.com>
2023-06-02 19:50:40 +0300

    Solve array wrapper

---
+++ js/arrayWrapper.js
@@ -0,0 +1,18 @@
+var ArrayWrapper = function(nums) {
+    this.nums = nums;
+};
+
+ArrayWrapper.prototype.valueOf = function() {
+    return this.nums.reduce((prev, curr) => prev + curr, 0)
+}
+
+ArrayWrapper.prototype.toString = function() {
+    return '[' + this.nums.toString() + ']';
+}
+
+
+ const obj1 = new ArrayWrapper([1,2]);
+ const obj2 = new ArrayWrapper([3,4]);
+ console.log(obj1 + obj2); // 10
+ console.log(String(obj1)); // "[1,2]"
+ console.log(String(obj2)); // "[3,4]"
